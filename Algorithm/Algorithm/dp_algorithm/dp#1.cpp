1e34519ae70cf7b1a3a8e9b381fe6e8c4f5ae9ef
yoonhyeokCho <hyeok6640@gmail.com>
2022-01-24 20:40:41 +0900

    동적 계획 알고리즘

---
+++ Algorithm/Algorithm/dp_algorithm/dp#1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[1000000]={0,};
+
+int main(){
+    int input;
+    cin>>input;
+
+    for(int i=2; i<=input; i++){
+        dp[i]=dp[i-1]+1;
+        if(i%2==0){
+            dp[i]=min(dp[i],dp[i/2]+1);
+        }
+        if(i%3==0){
+            dp[i]=min(dp[i],dp[i/3]+1);
+        }
+    }
+    cout<<dp[input]<<endl;
+    
+    return 0;
+}
