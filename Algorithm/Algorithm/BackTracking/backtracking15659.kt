ce4f364f03601f057526ada1df41e940bc7809cf
yoonhyeokCho <90389363+yoonhyeokCho@users.noreply.github.com>
2022-09-08 02:09:35 +0900

    Add files via upload

---
+++ Algorithm/Algorithm/BackTracking/backtracking15659.kt
@@ -0,0 +1,45 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStream
+import java.io.OutputStreamWriter
+import java.nio.Buffer
+import java.util.StringTokenizer
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+var bw = BufferedWriter(OutputStreamWriter(System.out))
+
+var n = 0
+var m = 0
+val check = BooleanArray(9)
+
+fun main(){
+    val token = StringTokenizer(br.readLine()," ")
+    n = token.nextToken().toInt()
+    m = token.nextToken().toInt()
+
+    dfs("")
+
+    bw.flush()
+
+}
+
+
+fun dfs(str: String) {
+    if (str.length >= m) {
+        for (i in 0..str.length - 1) {
+            bw.write("${str[i]} ")
+        }
+        bw.write("\n")
+        return
+    }
+    for (i in 1..n) {
+        if (check[i])
+            continue
+        check[i] = true
+        dfs(str + "${i}")
+        check[i] = false
+    }
+}
+
+
