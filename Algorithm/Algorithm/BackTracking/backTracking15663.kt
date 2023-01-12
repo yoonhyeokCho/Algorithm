8736ab1a548648072356bf2be5ded5e41baad36d
yoonhyeokCho <90389363+yoonhyeokCho@users.noreply.github.com>
2023-01-12 16:47:25 +0900

    [백트래킹] 백준 - 15663

---
+++ Algorithm/Algorithm/BackTracking/backTracking15663.kt
@@ -0,0 +1,55 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+
+private lateinit var arr: IntArray
+private lateinit var answer: IntArray
+private lateinit var visited: BooleanArray
+private lateinit var check:IntArray
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private val bw = BufferedWriter(OutputStreamWriter(System.out))
+private var n = 0
+private var m = 0
+fun main() {
+    var token = StringTokenizer(br.readLine(), " ")
+    n = token.nextToken().toInt()
+    m = token.nextToken().toInt()
+
+    arr = IntArray(n + 1)
+    visited = BooleanArray(n + 1)
+    answer = IntArray(m + 1)
+    token = StringTokenizer(br.readLine(), " ")
+    for (i in 0 until n) {
+        arr[i] = token.nextToken().toInt()
+    }
+
+    arr.sort()
+    check = IntArray(arr[n]+1)
+    dfs(0)
+    bw.flush()
+    bw.close()
+}
+
+private fun dfs(depth: Int) {
+
+    if (depth == m) {
+        for (i in 0 until m) {
+            bw.write(answer[i].toString() + " " )
+        }
+        bw.write("\n")
+        return
+    }
+    var count =0
+    for (i in 1..n) {
+
+        if(!visited[i] && count != arr[i]){
+            count = arr[i]
+            visited[i] = true
+            answer[depth] = arr[i]
+            dfs(depth + 1)
+            visited[i] = false
+        }
+    }
+}
